9bb539d4ce36a4a5e80fab21ca8c4763d04208e3
Jake Narkizian <Jake.Narkizian@gmail.com>
2016-10-20 23:06:41 -0700

    Added empty iOS project (resolves #21)

---
+++ ios/sound-send/sound-send/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sound-send
+//
+//  Created by Jake Narkizian on 10/20/16.
+//  Copyright © 2016 Jake Narkizian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
